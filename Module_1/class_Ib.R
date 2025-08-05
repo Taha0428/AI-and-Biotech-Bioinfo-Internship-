1a2031947c3121e06e21216a83fca0933f16c67f
Taha0428 <tahaj0336@gmail.com>
2025-08-05 09:59:39 +0500

    Update class_Ib.R

--- Module_1/class_Ib.R
+++ Module_1/class_Ib.R
@@ -1,4 +1,3 @@
-# Replace path with your actual directory path
 setwd("C:/Users/tahaj/Documents/AI_Omics_Internship_2025")
 getwd()  # Confirm working directory
 
@@ -8,21 +7,18 @@ dir.create("scripts")
 dir.create("results")
 dir.create("plots")
 
-# Load the CSV from raw_data
+# to load the CSV from raw_data
 data <- read.csv("raw_data/patient_info.csv")
 
-# View structure and first few rows
+# View str and first few rows
 str(data)
 head(data)
 
-
-# Convert gender to factor
 data$gender <- as.factor(data$gender)
 
 # Convert diagnosis to factor
 data$diagnosis <- as.factor(data$diagnosis)
 
-# Convert smoker to binary factor: 1 = Yes, 0 = No
 data$smoking_binary <- ifelse(data$smoker == "Yes", 1, 0)
 data$smoking_binary <- as.factor(data$smoking_binary)
 
@@ -30,5 +26,6 @@ data$smoking_binary <- as.factor(data$smoking_binary)
 str(data)
 
 write.csv(data, file = "clean_data/patient_info_clean.csv", row.names = FALSE)
+
 
 
